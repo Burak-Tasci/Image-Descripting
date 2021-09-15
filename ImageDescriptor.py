4d372336b3254d8988fc244ae277e24e3dd5385b
Burak Tasci <buraaktascii@outlook.com>
2021-09-15 15:00:15 +0300

    Final

--- ImageDescriptor.py
+++ ImageDescriptor.py
@@ -1,3 +1,59 @@
 import numpy as np
 import pandas as pd
 import cv2
+import imutils
+
+class ImageDescriptor:
+    def __init__(self,bins):
+        self.bins = bins
+    def describe(self, image):
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        features = []
+        (h, w) = image.shape[:2]
+        (top_cX, top_cY) = (int(w * 0.25), int(h * 0.30))
+        (bottom_cX, bottom_cY) = (int(w * 0.75), int(h * 0.60))
+        centerX = int(w / 2)
+        centerY = int(h / 2)
+
+        bottom_coordinates = (centerX, bottom_cY)
+        black = np.zeros((h,w), dtype = np.uint8)
+        bottom_ellipse_mask = cv2.ellipse(black, bottom_coordinates, (100, 100), 0,
+                          0, 360, (255, 255, 255), -1)
+        top_coordinates = (centerX, top_cY)
+        black = np.zeros((h,w), dtype = np.uint8)
+        top_ellipse_mask = cv2.ellipse(black, top_coordinates, (100, 100), 0,
+                          0, 360, (255, 255, 255), -1)
+        
+        ### First masked image with ellipse areas
+        top_and_bottom_mask = cv2.add(top_ellipse_mask, bottom_ellipse_mask)
+        ellipse_masked_image = cv2.bitwise_and(image,image,mask = top_and_bottom_mask)
+
+        left_top_corner = (centerX - 100,top_cY - 100)
+        right_bottom_corner = (centerX + 100, bottom_cY + 100)
+        black = np.zeros((h,w), dtype = np.uint8)
+        rect_mask = cv2.rectangle(black,left_top_corner,
+                         right_bottom_corner,
+                          (255,255,255), -1)
+
+        substracted_rect_and_ellipse_mask = cv2.subtract(rect_mask,top_and_bottom_mask)
+        ### Second masked image without ellipse areas
+        substracted_rect_and_ellipse_mask_image = cv2.bitwise_and(image,image,mask = substracted_rect_and_ellipse_mask)
+        hist = self.histogram(image, top_and_bottom_mask)
+        features.extend(hist)
+        return features
+
+    def histogram(self, image, mask):
+		# extract a 3D color histogram from the masked region of the
+		# image, using the supplied number of bins per channel
+        hist = cv2.calcHist([image], [0, 1, 2], mask, self.bins,
+			[0, 180, 0, 256, 0, 256])
+		# normalize the histogram if we are using OpenCV 2.4
+        if imutils.is_cv2():
+	        hist = cv2.normalize(hist).flatten()
+		# otherwise handle for OpenCV 3+
+        else:
+	        hist = cv2.normalize(hist, hist).flatten()
+		# return the histogram
+        return hist
+        
+
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# Image Descriptor
-This is a side project to use for any image search engine projects.
+# Image Search Engine
+## Root Directory Structure
+- data
+- queries
+- indexes
+- results
+- index.py
+- search.py
+- searcher.py
+- ImageDescriptor.py
+
+## How To Use?
+### 1. Indexing
+When you've prepared your dataset, you're ready to make indexing which is index.py's job. Here is how you use index.py:
+`python index.py -d .\data\ -i .\indexes\WhatYouName.csv`
+Note: You can use --index/--dataset instead of -i/-d.
+### 2.Searching
+At first step, we got the index file. Now you're ready to make a search. Decide which image would you like to use for query. If everything is OK, lets go!
+`python search.py -i .\indexes\WhatYouName.csv -q .\queries\QueryImage.jpg -r .\results\`
+
+Note: You can use --index/--query/--results instead of -i/-d/-r.
+<font color="orange">Warning:</font><font color="black"> Results directory cleans itself every time search.py runs.</font>
+A search example:
+**Query Image:**
+![queryimage](https://i.hizliresim.com/hhtyu20.jpg)
+
+**Results:**
+<table>
+  <tr >
+    <td> <img src="https://i.hizliresim.com/hhtyu20.jpg" width = 200></td>
+    <td><img src="https://i.hizliresim.com/ke4p72v.png" width="200"></td>
+	<td><img src="https://i.hizliresim.com/8fem1eg.png" width="200"></td>
+	<td><img src="https://i.hizliresim.com/g9jkfj6.png" width="200"></td>
+   </tr> 
+   <tr>
+    <td> <img src="https://i.hizliresim.com/n8l6fgn.png" width = 200></td>
+    <td><img src="https://i.hizliresim.com/toqiyfd.png" width="200"></td>
+	<td><img src="https://i.hizliresim.com/ro7baaw.png" width="200"></td>
+	<td><img src="https://i.hizliresim.com/or4n7oa.png" width="200"></td>
+  </tr>
+</table>
+
+
